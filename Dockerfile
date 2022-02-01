923ab4fd2c08098875e90cabeb15020f8200e248
Tamal2017 <tingangt@gmail.com>
2022-02-01 10:19:24 -0500

    Adding  docker-compose file

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM node:14-alpine
 
 ENV MONGO_DB_USERNAME=admin \
-    MONGO_DB_PWD=admin
+    MONGO_DB_PWD=password
 
 RUN mkdir -p /home/app
 
--- app/server.js
+++ app/server.js
@@ -6,7 +6,7 @@ const fs = require('fs');
  * 1. Replace the url value by 'mongodb://admin:password@mongodb' if you want to run app with Docker-compose or
  * 2. Replace the url value by 'mongodb://admin:password@localhost:27017' if you want to run app localy 
  */
-const url = 'mongodb://admin:password@mongodb';
+const url = 'mongodb://admin:password@localhost:27017';
 const dbName = 'user-account';
 const app = express();
 
@@ -14,9 +14,9 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(3000, function() {
-    console.log('=====================================');
-    console.log('App is up and listening on port 3000');
-    console.log('=====================================');
+    console.log('======================================');
+    console.log('App is up and listening on port : 3000');
+    console.log('======================================');
 });
 // Get
 app.get('/', (req, res) => {
@@ -25,17 +25,17 @@ app.get('/', (req, res) => {
 
 app.post('/users', (req, res) => {
     MongoClient.connect(url, (err, client) => {
-        if (err) return console.error('>>>> CONNECTION faild: ', err);
+        if (err) return console.error('>>>> CONNECTION faild : ', err);
         db = client.db(dbName);
         const usersCollection = db.collection('users');
         usersCollection.insertOne(req.body).then(_result => {
             res.redirect('/');
-        }).catch(error => console.error('>>>> POST Error: ', error));
+        }).catch(error => console.error('>>>> POST Error : ', error));
     });
 });
 
 // loading image
-app.get('/img', function(req, res) {
+app.get('/app/img', function(req, res) {
     var img = fs.readFileSync('mongo.jpg');
     res.write(200, { 'Content-Type': 'image/jpg' });
     res.end(img, 'binary');
